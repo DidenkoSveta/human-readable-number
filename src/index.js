5510606d7724e2ae23f10564ffbcfab51e6d2370
DidenkoSveta <106669577+DidenkoSveta@users.noreply.github.com>
2023-02-11 00:27:31 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,7 @@
-
-const assert = require('assert');
-
-function toReadable(number) {
-  switch (number) {
-    case 0:
-      return 'zero';
-    case 1:
-      return 'one';
-    default:
-      return number;
+module.exports = function toReadable (number) {
+  if (number === 0) {
+    return 'zero';
+  } else if (number === 1) {
+    return 'one';
   }
-}
-
-it('Should return \'zero\' when 0 given', () => {
-  const readable = toReadable(0);
-  assert.equal(readable, 'zero');
-});
-it('Should return \'one\' when 1 given', () => {
-  const readable = toReadable(1);
-  assert.equal(readable, 'one');
-});
+};
