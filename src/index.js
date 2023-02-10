d6bd51dca375dccd576295f37c6780cde43fa51c
DidenkoSveta <106669577+DidenkoSveta@users.noreply.github.com>
2023-02-11 00:25:26 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,107 +1,22 @@
+
+const assert = require('assert');
+
 function toReadable(number) {
-  let readable;
   switch (number) {
     case 0:
-      readable = 'zero';
-      break;
+      return 'zero';
     case 1:
-      readable = 'one';
-      break;
-    case 2:
-      readable = 'two';
-      break;
-    case 3:
-      readable = 'three';
-      break;
-    case 4:
-      readable = 'four';
-      break;
-    case 5:
-      readable = 'five';
-      break;
-    case 6:
-      readable = 'six';
-      break;
-    case 7:
-      readable = 'seven';
-      break;
-    case 8:
-      readable = 'eight';
-      break;
-    case 9:
-      readable = 'nine';
-      break;
-    case 10:
-      readable = 'ten';
-      break;
-    case 11:
-      readable = 'eleven';
-      break;
-    case 12:
-      readable = 'twelve';
-      break;
-    case 13:
-      readable = 'thirteen';
-      break;
-    case 14:
-      readable = 'fourteen';
-      break;
-    case 15:
-      readable = 'fifteen';
-      break;
-    case 16:
-      readable = 'sixteen';
-      break;
-    case 17:
-      readable = 'seventeen';
-      break;
-    case 18:
-      readable = 'eighteen';
-      break;
-    case 19:
-      readable = 'nineteen';
-      break;
-    case 20:
-      readable = 'twenty';
-      break;
-    case 30:
-      readable = 'thirty';
-      break;
-    case 40:
-      readable = 'forty';
-      break;
-    case 50:
-      readable = 'fifty';
-      break;
-    case 60:
-      readable = 'sixty';
-      break;
-    case 70:
-      readable = 'seventy';
-      break;
-    case 80:
-      readable = 'eighty';
-      break;
-    case 90:
-      readable = 'ninety';
-      break;
+      return 'one';
     default:
-      if (number < 100) {
-        readable = toReadable(number - number % 10) + '-' + toReadable(number % 10);
-      } else if (number < 1000) {
-        readable = toReadable(number - number % 100) + ' ' + toReadable(number % 100);
-      } else if (number < 1000000) {
-        readable = toReadable(number - number % 1000) + ' ' + toReadable(number % 1000);
-      } else if (number < 1000000000) {
-        readable = toReadable(number - number % 1000000) + ' ' + toReadable(number % 1000000);
-      } else if (number < 1000000000000) {
-        readable = toReadable(number - number % 1000000000) + ' ' + toReadable(number % 1000000000);
-      } else if (number < 1000000000000000) {
-        readable = toReadable(number - number % 1000000000000) + ' ' + toReadable(number % 1000000000000);
-      } else if (number < 1000000000000000000) {
-        readable = toReadable(number - number % 1000000000000000) + ' ' + toReadable(number % 1000000000000000);
-      }
+      return number;
   }
-  return readable;
 }
-module.exports = toReadable;
+
+it('Should return \'zero\' when 0 given', () => {
+  const readable = toReadable(0);
+  assert.equal(readable, 'zero');
+});
+it('Should return \'one\' when 1 given', () => {
+  const readable = toReadable(1);
+  assert.equal(readable, 'one');
+});
